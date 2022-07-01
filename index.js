44b42d70b1af1c8e5fde16ff4f14a89cd4bafeda
Michael Law <rmdashrfv@hey.com>
2022-07-01 15:31:08 -0400

    Added DND request and class selector dropdown even more

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const classSelector = document.getElementById('class-select')
+const baseImgUrl = 'https://www.dndbeyond.com/attachments/thumbnails/0/'
+const portraitMap = {
+  'fighter': '697/400/475/c3fighterintro.png', 
+  'wizard': '717/400/484/c3wizardintro.png',
+  'barbarian': '679/400/417/c3barbarianintro.png'
+}
 
 let request = async () => {
   // initiate request to dnd server
@@ -10,6 +16,17 @@ let request = async () => {
     option.setAttribute('value', char.name)
     option.innerText = char.name
     classSelector.append(option)
+  })
+
+  classSelector.addEventListener('change', (e) => {
+    document.getElementById('selected-class').innerText = e.target.value
+    let img = document.createElement('img')
+    img.id = 'char-img'
+    img.src = `${baseImgUrl}${portraitMap[e.target.value.toLowerCase()]}`
+    console.log(e.target.value)
+    img.setAttribute('width', '400px')
+    img.setAttribute('height', '400px')
+    document.body.append(img)
   })
 }
 
