27c1c738ffda61a48548441fe4118ca1f58b6b47
Michael Law <rmdashrfv@hey.com>
2022-07-01 16:21:18 -0400

    add all pictures

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const classSelector = document.getElementById('class-select')
 const baseImgUrl = 'https://www.dndbeyond.com/attachments/thumbnails/0/'
 const portraitMap = {
-  'fighter': '697/400/475/c3fighterintro.png', 
-  'wizard': '717/400/484/c3wizardintro.png',
-  'barbarian': '679/400/417/c3barbarianintro.png'
+  'barbarian': '679/400/417/c3barbarianintro.png',
+  'bard': '684/400/406/c3bardintro.png',
+  'cleric': '687/380/437/c3clericintro.png',
+  'druid': '693/400/399/c3druidintro.png',
+  'fighter':'697/400/475/c3fighterintro.png',
+  'paladin': '701/400/473/c3paladinintro.png',
+  'monk': '700/400/490/c3monkintro.png',
+  'ranger': '707/400/444/c3rangerintro.png',
+  'rogue': '709/375/480/c3rogueintro.png',
+  'sorcerer':'712/400/517/c3sorcererintro.png',
+  'wizard':'717/400/484/c3wizardintro.png',
+  'warlock':'716/400/512/c3warlockintro.png'
 }
 
 let request = async () => {
